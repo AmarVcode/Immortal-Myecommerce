f97acfe3c02bd6ba4b745d049dbf9c71773aacd4
amaryamunavishwakarma <98891008+amaryamunavishwakarma@users.noreply.github.com>
2022-11-16 02:10:29 +0530

    navbar updated

---
+++ my-app/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './myComponents/navbar/navbar.component';
+import { FooterComponent } from './myComponents/footer/footer.component';
+import { HomeComponent } from './myComponents/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { 
+}
