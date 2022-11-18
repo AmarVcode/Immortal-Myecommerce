3bae6467e578b1f2e128170bb8e7d7f9901d2aa5
Amar <108946547+AmarVcode@users.noreply.github.com>
2022-11-18 21:20:15 +0530

    updatyed

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormComponent } from './myComponents/form/form.component';
+import { HomeComponent } from './myComponents/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'SignIn', component: FormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
