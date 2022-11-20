e626f4f22de235cb9504ef2e2720599b476a9b8b
Amar <108946547+AmarVcode@users.noreply.github.com>
2022-11-20 23:06:07 +0530

    bug fix

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FavoritesComponent } from './myComponents/favorites/favorites.component';
 import { FormComponent } from './myComponents/form/form.component';
 import { HomeComponent } from './myComponents/home/home.component';
+import { ProductsComponent } from './myComponents/products/products.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'SignIn', component: FormComponent }
+  { path: 'SignIn', component: FormComponent },
+  { path: 'Products', component: ProductsComponent },
+  { path: 'Favorites', component: FavoritesComponent }
 ];
 
 @NgModule({
