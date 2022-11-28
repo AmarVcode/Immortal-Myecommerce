d80e9e59188b382c984be4cc7305870cefd6385d
@amarvcode <amarvcode@gmail.com>
2022-11-28 15:12:53 +0530

    favorite completed

--- my-app/src/app/myComponents/favorites/favorites.component.ts
+++ my-app/src/app/myComponents/favorites/favorites.component.ts
@@ -6,23 +6,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./favorites.component.css']
 })
 export class FavoritesComponent implements OnInit {
-
-array:any=localStorage.getItem("favourite") || []
+public title="Your Favorites Are Here"
+array:any=localStorage.getItem("fav") || []
 
   constructor() { 
+    if(this.array.length>0){
+      this.array=JSON.parse(this.array)
+    }
 console.log(this.array)
+  }
+
+deleted(){
+
+  let empty:any=[]
+  empty=JSON.stringify(empty)
+
+  localStorage.setItem("fav",empty)
+  window.location.reload()
+}
 
 
-localStorage.setItem("favourite",this.array)
+
+
+  //fixed Function Image Zoom
+  fixed(src: any) {
+    console.log("working")
+    console.log(src)
+    let div = document.getElementById("fixed")
+    div?.setAttribute("class", "fixed2")
+    let img = document.getElementById("zoomed")
+    img?.setAttribute("src", src)
+  }
+
+
+  //removing function for nmoveing class from dom reverse of fixed
+  removefixed() {
+    let div = document.getElementById("fixed")
+    div?.classList.remove("fixed2")
   }
 
 
 
-
-
-
-
-
+   //Detail Function --------------------------
+   detail(items: any) {
+    items = JSON.stringify(items)
+    localStorage.setItem("detail", items)
+    location.pathname = "productdetail"
+  }
 
 
 
--- my-app/src/app/myComponents/home/home.component.ts
+++ my-app/src/app/myComponents/home/home.component.ts
@@ -11,45 +11,45 @@ import { OfferService } from 'src/app/Service/offer.service';
 export class HomeComponent implements OnInit {
 
   public title = "Value For Money!"
-  offer:any=[]
+  offer: any = []
 
 
 
   //fetching Data From Service
-  constructor(private offerdate:OfferService) {
-    
-    this.offer =offerdate.Offer()
+  constructor(private offerdate: OfferService) {
+
+    this.offer = offerdate.Offer()
     console.log(this.offer)
   }
- 
+
 
 
 
 
   //Detail Function --------------------------
-  detail(items:any){
-    items=JSON.stringify(items)
-localStorage.setItem("detail",items)
-location.pathname="productdetail"
+  detail(items: any) {
+    items = JSON.stringify(items)
+    localStorage.setItem("detail", items)
+    location.pathname = "productdetail"
   }
-  
+
 
 
 
   //fixed Function Image Zoom
-  fixed(src:any){
+  fixed(src: any) {
     console.log("working")
     console.log(src)
-    let div=document.getElementById("fixed")
-    div?.setAttribute("class","fixed2")
-    let img=document.getElementById("zoomed")
-    img?.setAttribute("src",src)
+    let div = document.getElementById("fixed")
+    div?.setAttribute("class", "fixed2")
+    let img = document.getElementById("zoomed")
+    img?.setAttribute("src", src)
   }
 
 
-//removing function for nmoveing class from dom reverse of fixed
-  removefixed(){
-    let div=document.getElementById("fixed")
+  //removing function for nmoveing class from dom reverse of fixed
+  removefixed() {
+    let div = document.getElementById("fixed")
     div?.classList.remove("fixed2")
   }
 
@@ -57,40 +57,26 @@ location.pathname="productdetail"
 
 
   //addfav function for adding favourites -------------
-  addfav(items:any){
-
-let store:any=localStorage.getItem("fav")||[]
-if(store.length === 0){
-  store.push(items)
-  console.log("added")
-}
-else{
-  store=JSON.parse(store)
-
-
-  for(let i=0;i<store.length;i++){
-    if(store[i].mini_title===items.mini_title){
-      console.log("item Already Exist");
-    }
-    else{
+  addfav(items: any) {
+    let store: any = localStorage.getItem("fav") || []
+    if (store.length === 0) {
       store.push(items)
-      break;
+      console.log("added")
     }
-  }
+    else {
+      store = JSON.parse(store)
+      store.push(items)
 
-}
-
-
-
-console.log(store)
-store=JSON.stringify(store)
-localStorage.setItem("fav",store)
+    }
+    console.log(store)
+    store = JSON.stringify(store)
+    localStorage.setItem("fav", store)
 
 
   }
 
   ngOnInit(): void {
-    
+
   }
 
 }
