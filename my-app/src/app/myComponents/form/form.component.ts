375520a6efcd65c51e82ceb87cd7d970186df307
@amarvcode <amarvcode@gmail.com>
2022-11-29 18:51:53 +0530

    completed

--- my-app/src/app/myComponents/form/form.component.ts
+++ my-app/src/app/myComponents/form/form.component.ts
@@ -20,14 +20,14 @@ export class FormComponent implements OnInit {
   
   signInUser(){
     console.log(this.signIn.value.user)
-    if(this.signIn.value.user !== null && this.signIn.value.userLast !== null && this.signIn.value.userMiddle !== null){
-      localStorage.setItem("userName",this.signIn.value.user)
-      alert("Thanks For Sign In Our Website, Confirm Your Sign in " + this.signIn.value.user +" "+ this.signIn.value.userMiddle +" "+ this.signIn.value.userLast)
-      window.location.href="/Home"
+    if(this.signIn.value.user===null){
+      localStorage.setItem("userName","Sign In")
     }
     else{
-      confirm("Fill The Below Form")
+      localStorage.setItem("userName",this.signIn.value.user)
     }
+      alert("Thanks For Sign In Our Website, Confirm Your Sign in " + this.signIn.value.user +" "+ this.signIn.value.userMiddle +" "+ this.signIn.value.userLast)
+      location.reload()
   }
 
   
