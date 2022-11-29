422e99adc07fd363178070a40fc470475b9a3b0f
@amarvcode <amarvcode@gmail.com>
2022-11-29 08:33:52 +0530

    footer done

---
+++ my-app/src/app/Service/allproduct.service.ts
@@ -0,0 +1,161 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AllproductService {
+  static Product: any;
+
+  constructor() { }
+
+  Product() {
+    return [
+      {
+        mini_title: "Ambrane Trone Wired Gaming Mouse",
+        main_title: "Ambrane Trone Wired Gaming Mouse with 7 Programmable Buttons, 3-Speed Customizable 3600 DPI, Comfortable Grip, Breathing LED Lighting, Fast Response Time, 1.5m Long USB Cable, Lightweight (Black) ",
+        brand: "Ambrane",
+        price: 399,
+        delivery: 40,
+        typeOfProduct: "Gaming",
+        src: "https://www.amazon.in/Ambrane-Trone-Programmable-Customizable-Comfortable/dp/B0BGY2ZGND/ref=sr_1_22_sspa?crid=1YUKPT47O0MYO&keywords=gaming+association&qid=1669664972&qu=eyJxc2MiOiIxLjg4IiwicXNhIjoiMC4wMCIsInFzcCI6IjAuMDAifQ%3D%3D&sprefix=gaming+assec%2Caps%2C270&sr=8-22-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9tdGY&psc=1",
+        about: [
+          "7 Colour Breathing Lights: Light up the way you play your favourite games with vibrant and pleasing 7 colour breathing lights. ",
+          "Fast Response Time: Play games like a Pro with a fast response time so that every movement is timed perfectly. ",
+          "7 Multifunctional Keys: Take full control with 7 pre-programmed multifunctional keys that you can access while playing games and while completing tasks with ease.",
+          "DPI Level Selection: Adjust the DPI as per your requirements as this mouse comes with 3 DPI ranges- 1200/2400/3600. By default, it is set at 1200 DPI. ",
+          "Lightweight and Comfortable: Its ergonomic design fits perfectly in your palm and keeps your hand relaxed for prolonged gaming. Being as light as 120g, you can carry it anywhere you want.",
+          "High Precision: Its advanced sensors are built especially to provide high accuracy and precise positioning while you play ",
+          "1.5m Braided Copper Wire: Play carefree even from a distance with a 1.5m long braided USB wire. "
+        ],
+        main_img: "https://m.media-amazon.com/images/I/61AN0NBDzBL._SL1500_.jpg",
+        sub_img: [
+          "https://m.media-amazon.com/images/I/71PKCCuOuZL._SL1500_.jpg",
+          "https://m.media-amazon.com/images/I/71HVCLtFgvL._SL1500_.jpg",
+          "https://m.media-amazon.com/images/I/713A3v3GODL._SL1500_.jpg",
+          "https://m.media-amazon.com/images/I/71+fh6iCHuL._SL1500_.jpg",
+          "https://m.media-amazon.com/images/I/715Z4PqXSfL._SL1500_.jpg"
+        ]
+      },
+      {
+        mini_title: "Redragon K617 Fizz 60% Wired RGB Gaming Keyboard",
+        main_title: "Redragon K617 Fizz 60% Wired RGB Gaming Keyboard, 61 Keys Compact Mechanical Keyboard w/White and Grey Color Keycaps, Linear Red Switch, Pro Driver/Software Supported ",
+        brand: "Redragon",
+        price: 2899,
+        delivery: "Free",
+        typeOfProduct: "Gaming",
+        src: "https://www.amazon.in/Redragon-K617-Keyboard-Mechanical-Supported/dp/B09BVCVTBC/ref=sr_1_5?crid=1AJHU0ZB2VAX0&keywords=gaming%2Bkeyboard&qid=1669666208&qu=eyJxc2MiOiI1Ljg3IiwicXNhIjoiNS41NCIsInFzcCI6IjQuNzkifQ%3D%3D&sprefix=gaming%2Bkey%2Caps%2C361&sr=8-5&th=1",
+        about: [
+          "Slim Fresh K617: Redragon's new 60% layout wired keyboard in a refreshing style with a no-brainer deal economic offer. Compact 61 keys with selected keycaps, dedicated for FPS Gamers and efficient working. ",
+          "Hot-Swappable Red Switches: Most quiet mechanical switch, linear and soft key travel makes every click easy to register. Hot-swappable with other Redragon switches. Made to last with switches rated for 50 million keypresses. ",
+          "Vibrant RGB: Up to 20 presets backlighting modes are free to choose by the keyboard itself. Brightness and flowing speed is also adjustable on board. Select your own preferred modes for any playing. ",
+          "Pro Software Customizable: Expand your options using the available software to design your own new modes and effects found on redragonshop. Macros with different keybindings or shortcuts for more efficient work and gaming. ",
+          "Dedicated for FPS Gamer: Place the keyboard proper straight on your desktop and no more crooked way for mouse space saving, your mouse will never hit the keyboard any more. Enjoy waving the mouse without any worries and go get that Team Kills."
+        ],
+        main_img: "https://m.media-amazon.com/images/I/6124OpB-LRL._SL1500_.jpg",
+        sub_img: [
+          "https://m.media-amazon.com/images/I/61OjTmn08ZL._SL1500_.jpg",
+          "https://m.media-amazon.com/images/I/71o55M0YlRL._SL1500_.jpg",
+          "https://m.media-amazon.com/images/I/71BXoIitCUL._SL1500_.jpg",
+          "https://m.media-amazon.com/images/I/71IkjP74ALL._SL1500_.jpg",
+          "https://m.media-amazon.com/images/I/61G4BdnpXdL._SL1500_.jpg",
+          "https://m.media-amazon.com/images/I/61E4axlXkOL._SL1500_.jpg",
+          "https://m.media-amazon.com/images/I/61zLB-yPK+L._SL1000_.jpg"
+        ]
+      },
+      {
+        mini_title: "Redgear Blaze Semi-Mechanical wired Gaming keyboard ",
+        main_title: "Redgear Blaze Semi-Mechanical wired Gaming keyboard with 3 colour backlit, full aluminium body & Windows key lock for PC ( Black )",
+        brand: " 	Redgear",
+        price: 999,
+        delivery: "Free",
+        typeOfProduct: "Gaming",
+        src: "https://www.amazon.in/Redgear-Blaze-backlit-keyboard-aluminium/dp/B073QQR2H2/ref=sr_1_omk_4?crid=1AJHU0ZB2VAX0&keywords=gaming+keyboard&qid=1669666208&qu=eyJxc2MiOiI1Ljg3IiwicXNhIjoiNS41NCIsInFzcCI6IjQuNzkifQ%3D%3D&sprefix=gaming+key%2Caps%2C361&sr=8-4",
+        about: [
+          "Semi-Mechanical Keyboard with Floating Key caps, greater durability and responsiveness.Interface: Wired USB ",
+          "3 Color mode for your gaming setup needs. Illuminated Keys : Yes ",
+          "19 keys anti-ghost for your gamers and programmers ",
+          "Windows key-lock function to avoid in-game pop up "
+        ],
+        main_img: "https://m.media-amazon.com/images/I/61M2CPqMgwL._SL1000_.jpg",
+        sub_img: [
+          "https://m.media-amazon.com/images/I/61mNn-SbK-L._SL1000_.jpg",
+          "https://m.media-amazon.com/images/I/81vnT4N6H6L._SL1500_.jpg",
+          "https://m.media-amazon.com/images/I/71R3YlFqaiL._SL1500_.jpg",
+          "https://m.media-amazon.com/images/I/41WwtSt4D+L.jpg"
+        ]
+      },
+      {
+        mini_title: "Acer Nitro QG221Q 21.5 Inch (54.61 cm) Backlit Led 1920 X 1080 Pixels Full HD Gaming Monitor",
+        main_title: "Acer Nitro QG221Q 21.5 Inch (54.61 cm) Backlit Led 1920 X 1080 Pixels Full HD Gaming Monitor I Va Panel I 1 MS VRB, 75 Hz Refresh Rate I 250 Nits Brightness I AMD Free Sync I Eye Care Features (Black) ",
+        brand: "Acer",
+        price: 7999,
+        delivery: "Free",
+        typeOfProduct: "Gaming",
+        src: "https://www.amazon.in/Acer-Backlit-Monitor-Brightness-Features/dp/B0BDS7JGMX/ref=sr_1_10?crid=VOWC3HO3LH80&keywords=monitor&qid=1669667648&qu=eyJxc2MiOiI2Ljc2IiwicXNhIjoiNi42NiIsInFzcCI6IjUuOTUifQ%3D%3D&sprefix=moni%2Caps%2C254&sr=8-10",
+        about: [
+          "Exceptional Full HD 21.5 Inch Display I Enjoy immaculate image quality with 1920x1080 resolution I 178 degree wide viewing angles I Robust stand design with strong stability ",
+          "1 MS VRB Respose time helps Reduces Ghost & smearing effect also Elimination blur on fast moving object I 75 Hz Refresh Rate for Outstanding fluidity of images & also provide gamers an excellent in-game experience ",
+          "Integrated AMD FreeSync technology for Enabling smooth gameplay, Eliminating screen tearing, Minimizing lag and latency and delivers very smooth gaming experiences. ",
+          "Connectivity Options Includes : 1 X VGA 2 X HDMI (1.4) Ports with Inbox HDMI Cable I Wall Mount Ready (100x100 mm) I -5 to 20 degree tilted angle for the comfortable viewing ",
+          "Care for your eyes with Bluelight Shield, Flickerless, Low Dimming, ComfyView Disply I Acer Game Modes to optimize the visuals for different types of content: Action, Racing, Sport, User, Standard, ECO, Graphic and Movie."
+        ],
+        main_img: "https://m.media-amazon.com/images/I/81SPzVl3GxL._SL1500_.jpg",
+        sub_img: [
+          "https://m.media-amazon.com/images/I/61KSpTwo7EL._SL1080_.jpg",
+          "https://m.media-amazon.com/images/I/71DNpwdT+WL._SL1080_.jpg",
+          "https://m.media-amazon.com/images/I/71JOgPOFbPL._SL1080_.jpg",
+          "https://m.media-amazon.com/images/I/61lHRWzRVhL._SL1080_.jpg",
+          "https://m.media-amazon.com/images/I/41fnsWoAQjL._SL1024_.jpg"
+        ]
+      },
+      {
+        mini_title: "Sunon Gaming Chair",
+        main_title: "Sunon Gaming Chair, Computer Chair with Footrest and Lumbar Support, Height Adjustable Gaming Chair with 360°-Swivel Seat and Headrest for Office or Gaming",
+        brand: "Sunon",
+        price: 10599,
+        delivery: "Free",
+        typeOfProduct: "Gaming",
+        src: "https://www.amazon.in/Sunon-Adjustable-Leather-Retractable-Footrest/dp/B09MFHHT3Q/ref=sr_1_17?crid=4JQTHHRZ9UWC&keywords=gaming+chair&qid=1669668388&qu=eyJxc2MiOiI3LjMyIiwicXNhIjoiNy4xNCIsInFzcCI6IjUuNTkifQ%3D%3D&sprefix=gaming%2Caps%2C395&sr=8-17",
+        about: [
+          "Primary Material: Solid wood, Upholstery Material: Fabric, Top Material: Wood ",
+          "WARRANTY DETAILS - 1 Year Limited Brand Warranty for Manufacturing Defects only. For any kind of more product info, Product Customization, after sale service and to claim Warranty please reach us.; Assembly Instructions: Seller Will Provide. ",
+          "These products are shot at location at Casaliving studios and adhere to strict manufacturing processes and quality assurance and eligible for return according to guidelines. Get the best in class products made of quality materials for long term furniture needs. ",
+          "Color: Grey, Style: Modern, Seating Capacity: 6 seater. ",
+          "Back Style: Cushion Back; Item Shape: L-Shape; Seating Capacity: 4.06.0; Special Features: Space Saving; Form Factor: Sectional; Size Name: Medium; Arm Style: Square "
+        ],
+        main_img: "https://m.media-amazon.com/images/I/61L6P-CvRGL._SL1500_.jpg",
+        sub_img: [
+          "https://m.media-amazon.com/images/I/81THoAVSH+L._SL1500_.jpg",
+          "https://m.media-amazon.com/images/I/71pIofRESSL._SL1500_.jpg",
+          "https://m.media-amazon.com/images/I/61cmbZ5GT0L._SL1500_.jpg",
+          "https://m.media-amazon.com/images/I/61L1aY23WSL._SL1500_.jpg",
+          "https://m.media-amazon.com/images/I/61+kxC3U59L._SL1500_.jpg"
+        ]
+      },
+      {
+        mini_title: "47 inch Gaming Desk Z-Shaped",
+        main_title: "47 inch Gaming Desk Z-Shaped Computer Desk PC Computer Table Home Office Desk Ergonomic Gamer Workstation with Headphone Hook, Red",
+        brand: "Dkelincs",
+        price: 8133,
+        delivery: "Free",
+        typeOfProduct: "Gaming",
+        src: "",
+        about: [
+          "Primary Material: Solid wood, Upholstery Material: Fabric, Top Material: Wood ",
+          "WARRANTY DETAILS - 1 Year Limited Brand Warranty for Manufacturing Defects only. For any kind of more product info, Product Customization, after sale service and to claim Warranty please reach us.; Assembly Instructions: Seller Will Provide. ",
+          "These products are shot at location at Casaliving studios and adhere to strict manufacturing processes and quality assurance and eligible for return according to guidelines. Get the best in class products made of quality materials for long term furniture needs. ",
+          "Color: Grey, Style: Modern, Seating Capacity: 6 seater. ",
+          "Back Style: Cushion Back; Item Shape: L-Shape; Seating Capacity: 4.06.0; Special Features: Space Saving; Form Factor: Sectional; Size Name: Medium; Arm Style: Square "
+        ],
+        main_img: "https://m.media-amazon.com/images/I/61L6P-CvRGL._SL1500_.jpg",
+        sub_img: [
+          "https://m.media-amazon.com/images/I/81THoAVSH+L._SL1500_.jpg",
+          "https://m.media-amazon.com/images/I/71pIofRESSL._SL1500_.jpg",
+          "https://m.media-amazon.com/images/I/61cmbZ5GT0L._SL1500_.jpg",
+          "https://m.media-amazon.com/images/I/61L1aY23WSL._SL1500_.jpg",
+          "https://m.media-amazon.com/images/I/61+kxC3U59L._SL1500_.jpg"
+        ]
+      }
+    ]
+  }
+}
--- my-app/src/app/myComponents/products/products.component.ts
+++ my-app/src/app/myComponents/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AllproductService } from 'src/app/Service/allproduct.service';
 
 @Component({
   selector: 'app-products',
@@ -6,8 +7,86 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
+  
+public title="All Products"
 
-  constructor() { }
+  offer: any = [];
+
+
+  //fetching Data From Service
+  constructor(private offerdate: AllproductService) {
+
+    this.offer = offerdate.Product()
+    console.log(this.offer)
+  }
+
+
+
+
+
+
+  //Detail Function --------------------------
+  detail(items: any) {
+    items = JSON.stringify(items)
+    localStorage.setItem("detail", items)
+    location.pathname = "productdetail"
+  }
+
+
+
+
+  //fixed Function Image Zoom
+  fixed(src: any) {
+    console.log("working")
+    console.log(src)
+    let div = document.getElementById("fixed")
+    div?.setAttribute("class", "fixed2")
+    let img = document.getElementById("zoomed")
+    img?.setAttribute("src", src)
+  }
+
+
+  //removing function for nmoveing class from dom reverse of fixed
+  removefixed() {
+    let div = document.getElementById("fixed")
+    div?.classList.remove("fixed2")
+  }
+
+
+
+
+  //addfav function for adding favourites -------------
+  
+  bool:any=true;
+  addfav(items: any) {
+    let store: any = localStorage.getItem("fav") || [];
+    if (store.length === 0) {
+      store.push(items);
+      console.log("added");
+    }
+    else{
+      store=JSON.parse(store);
+      store.map((element:any)=>{
+        if(element.brand===items.brand){
+          this.bool=false;
+        }
+      });
+
+console.log(this.bool);
+    
+    if(this.bool===true){
+      store.push(items);
+    }
+    else{
+      console.log("already Exist Items");
+    }
+
+    }
+    
+
+    store = JSON.stringify(store);
+    localStorage.setItem("fav", store);
+  }
 
   ngOnInit(): void {
   }
